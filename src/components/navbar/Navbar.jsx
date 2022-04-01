e60bd41a821eb9774f3c52a2b66652b3413b3d10
sushil-mod <78729065+sushil-mod@users.noreply.github.com>
2022-04-01 18:47:13 +0530

    Feat : added navbar component

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+
+function Navbar() {
+  return <>
+    <nav className='menu flex-space-btw' >          
+        <Link className='menu__link gap-sm  pri-text-color text-center flex-center' to="">
+            <i className='fas fa-jedi menu__link_logo pri-color ' ></i>
+            <span className='menu__link_title text-center'>Jedi Notes</span>                              
+        </Link>                            
+        <div className='menu__container gap-lg flex-space-btw '>
+            <div className='menu__container_link cur-pointer'>
+                <Link className='login_link pri-text-color'   to="">Login</Link>
+            </div>
+            <div className='menu__container_btn flex-center pri-bg-color '>
+                <button className='theme_btn cur-pointer'> <i class="fas fa-moon"></i> </button> 
+            </div>  
+        </div>
+      </nav>
+  </>
+}
+
+export default Navbar;
