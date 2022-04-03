1fca96d3007d531db8764e7f4fc3d70cd7d4498d
sushil-mod <78729065+sushil-mod@users.noreply.github.com>
2022-04-04 03:03:30 +0530

    fix : fixed imports

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+export { Login } from "./auth/Login";
+export {Signup } from "./auth/Signup";
+export { Home } from "./home/Home";
+export { Archive} from "./archive/Archive";
+export { MyNotes } from "./mynotes/MyNotes";
+export { Trash } from "./trash/Trash";
+export { Label } from "./label/Label";
