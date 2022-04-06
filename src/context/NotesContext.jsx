e80a2cbdf80637a7c7669875b10af0d7e4c35c83
sushil-mod <78729065+sushil-mod@users.noreply.github.com>
2022-04-06 23:32:33 +0530

    Feat : added Note Add and delete functionality

--- src/context/NotesContext.jsx
+++ src/context/NotesContext.jsx
@@ -1,27 +1,51 @@
-import React,{createContext,useContext,useState } from 'react';
+import axios from 'axios';
+import React,{createContext,useContext,useState ,useEffect} from 'react';
+import { useAuthorizer } from './AuthorizerContext';
+
 
 const NotesContext = createContext();
 
 const NotesProvider = ({children})=>{
-
+    const { authState } =useAuthorizer();
     const initialNote ={
         title:"",
         description:"",
-        priority:"",
+        priority:"none",
         color:"",
         pinStatus:false,
         tags:[],
-        createdDate: ""
+        createdDate: "",
+        trashStatus:false
     }
     
     const [ note ,setNote ] =useState(initialNote);
     const [ labelArray , setLabelArray ] =useState([]);
     const [bgColor,setBgColor] = useState("color-white");
-console.log(note);
+    const [noteList , setNoteList] = useState([]);
+
+    console.log(note);
+
+
+    useEffect(()=>{
+
+        (async()=>{
+            try {
+                const response = await axios.get("/api/notes",{headers:{authorization:authState.encodedToken}})
+                console.log(response,"response");
+            } catch (error) {
+                console.error(error)
+            }
+            
+        })();
+    },[authState])
+
+
+    console.log("note list",noteList);
+
 
 
     return<>
-        <NotesContext.Provider value={{ note ,setNote , labelArray , setLabelArray,bgColor,setBgColor  }}>
+        <NotesContext.Provider value={{ note ,setNote , labelArray , setLabelArray,bgColor,setBgColor ,noteList , setNoteList ,initialNote }}>
             {children}
         </NotesContext.Provider>
     </>
--- src/pages/mynotes/MyNotes.jsx
+++ src/pages/mynotes/MyNotes.jsx
@@ -1,24 +1,51 @@
 
 import React from 'react'
-
+import {CgNotes} from 'react-icons/cg'
 import CreateNoteCard from '../../components/card/CreateNoteCard';
+import { NoteCard } from '../../components/card/NoteCard';
+import { useAuthorizer } from '../../context/AuthorizerContext';
+import { useNotes } from '../../context/NotesContext';
 
 
 export const  MyNotes=()=> {
+  const { authState } = useAuthorizer();
+  const { noteList } =useNotes(); 
+    const filterNoteList = noteList ? noteList.filter((item) => item.trashStatus === false) : [] ;
+    const pinNoteList = filterNoteList ? filterNoteList.filter((item) => item.pinStatus === true ): [] ;
+    const otherNoteList = filterNoteList ? filterNoteList.filter((item) => item.pinStatus === false ):[] ;
+
+  console.log("from notes", noteList );
 
   return<>
-          {/* <div className='wd-100' >
+         { !authState.loginStatus ? <div className='wd-100' >
                   <div className='flex-center flex-col height-100 grey-shade' >
                     <div> <CgNotes size={"10rem"} /> </div>
                     <div> <h2>Notes you add appear here </h2> </div>
+                    <div><h2>Please Login From Navbar </h2></div>
                   </div>      
 
-          </div> */}
+          </div> :
           <div  className="wd-100 " >
 
             <CreateNoteCard/>
-            
-          </div>
+      
+                            <div className='wd-100' >
+
+                             {pinNoteList.length >0 && <div >
+                                <h2>pinned</h2> 
+                                <div style={{ display:"flex", flexWrap:"wrap" }}>
+                                { pinNoteList.map( (noteItem)=> <NoteCard noteItem={ noteItem } key={ noteItem._id } />) }
+                                </div>
+                              </div>
+                              }     
+                              <div >
+                               { pinNoteList.length > 0 ? <h2>others</h2> : <h2> Notes </h2>   }  
+                                <div className='d-flex-wrap'>
+                                { otherNoteList.map( (noteItem)=> <NoteCard noteItem={ noteItem } key={ noteItem._id } />) }
+                                </div>
+                              </div>
+                            </div> 
+          </div>}
   </>
 }
 
